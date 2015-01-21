c2236143cbc23f48db288a1bcd0776531df39ed9
Havi Lee <Lee@MacBook-Air.local>
2015-01-21 23:09:58 +0800

    Release 1.0.0

---
+++ LHMarqueeView/CustomerView.h
@@ -0,0 +1,13 @@
+//
+//  CustomerView.h
+//  demo
+//
+//  Created by Havi Lee on 15/1/21.
+//  Copyright (c) 2015年 haiwei_lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomerView : UIView
+
+@end
---
+++ LHMarqueeView/CustomerView.m
@@ -0,0 +1,21 @@
+//
+//  CustomerView.m
+//  demo
+//
+//  Created by Havi Lee on 15/1/21.
+//  Copyright (c) 2015年 haiwei_lee. All rights reserved.
+//
+
+#import "CustomerView.h"
+
+@implementation CustomerView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
